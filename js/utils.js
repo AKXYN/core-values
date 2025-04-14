2e27097d5adc50ebbd7a7dc98abf95bc0a2f8d58
AKXYN <97322192+AKXYN@users.noreply.github.com>
2025-04-14 08:50:15 +0530

    Trying to provide correct repo name in base path

--- js/utils.js
+++ js/utils.js
@@ -4,32 +4,43 @@
 function getBasePath() {
     // Check if we're running on GitHub Pages
     const isGitHubPages = window.location.host.includes('github.io');
+    console.log("Is GitHub Pages:", isGitHubPages);
+    console.log("Current host:", window.location.host);
+    console.log("Current pathname:", window.location.pathname);
     
     // If on GitHub Pages, use the repository name as the base path
-    // Update this to match your actual repository name
     const repoName = 'core-values';
+    const basePath = isGitHubPages ? `/${repoName}` : '';
     
-    return isGitHubPages ? `/${repoName}` : '';
+    console.log("Base path:", basePath);
+    return basePath;
 }
 
 // Get the full URL for a page
 function getPageUrl(pageName) {
-    return window.location.origin + getBasePath() + '/' + pageName;
+    const basePath = getBasePath();
+    const url = window.location.origin + basePath + '/' + pageName;
+    console.log("Generated URL:", url);
+    return url;
 }
 
 // Navigate to a page
 function navigateToPage(pageName) {
-    window.location.href = getPageUrl(pageName);
+    const url = getPageUrl(pageName);
+    console.log("Navigating to:", url);
+    window.location.href = url;
 }
 
 // Navigate to a page with query parameters
 function navigateToPageWithParams(pageName, params) {
-    const url = new URL(getPageUrl(pageName), window.location.origin);
+    const basePath = getBasePath();
+    const url = new URL(window.location.origin + basePath + '/' + pageName);
     
     // Add each parameter to the URL
     Object.keys(params).forEach(key => {
         url.searchParams.append(key, params[key]);
     });
     
+    console.log("Navigating to with params:", url.toString());
     window.location.href = url.toString();
 } 
